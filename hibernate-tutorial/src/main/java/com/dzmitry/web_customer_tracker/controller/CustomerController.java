b91626ff42d5b55b10f6a7895c9a765fecce3b22
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2022-08-14 18:58:16 +0200

    added a Service Layer

--- hibernate-tutorial/src/main/java/com/dzmitry/web_customer_tracker/controller/CustomerController.java
+++ hibernate-tutorial/src/main/java/com/dzmitry/web_customer_tracker/controller/CustomerController.java
@@ -3,9 +3,11 @@ package com.dzmitry.web_customer_tracker.controller;
 
 import com.dzmitry.web_customer_tracker.dao.CustomerDAO;
 import com.dzmitry.web_customer_tracker.entity.Customer;
+import com.dzmitry.web_customer_tracker.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -15,12 +17,12 @@ import java.util.List;
 public class CustomerController {
 
     @Autowired
-    private CustomerDAO customerDAO;
+    private CustomerService customerService;
 
-    @RequestMapping("/list")
+    @GetMapping("/list")
     public String listCustomers(Model model){
 
-        List<Customer> customers = customerDAO.getCustomers();
+        List<Customer> customers = customerService.getCustomers();
         model.addAttribute("customers", customers);
 
         return "list-customers";
---
+++ hibernate-tutorial/src/main/java/com/dzmitry/web_customer_tracker/service/CustomerService.java
@@ -0,0 +1,10 @@
+package com.dzmitry.web_customer_tracker.service;
+
+import com.dzmitry.web_customer_tracker.entity.Customer;
+
+import java.util.List;
+
+public interface CustomerService {
+
+    public List<Customer> getCustomers();
+}
